6b12caefb07985962ca5d8327abf1ebd3f021698
aandandreea <126575169+aandandreea@users.noreply.github.com>
2025-06-10 11:10:25 +0300

    Solved Percentage_of_users_atended_in_a_contest.sql

---
+++ Percentage_of_users_atended_in_a_contest.sql
@@ -0,0 +1,35 @@
+/* 1633.Percentage of users attended in a contest
+Table: Users
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| user_id     | int     |
+| user_name   | varchar |
++-------------+---------+
+user_id is the primary key (column with unique values) for this table.
+Each row of this table contains the name and the id of a user.
+ 
+
+Table: Register
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| contest_id  | int     |
+| user_id     | int     |
++-------------+---------+
+(contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+The result format is in the following example.*/
+select r.contest_id,
+  round(count(distinct r.user_id) * 100.0 / (select count(*) from users), 2) as percentage
+from register r
+group by r.contest_id
+order by percentage DESC,r.contest_id ASC;
