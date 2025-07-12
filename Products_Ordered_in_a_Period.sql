5b230ca57625cfc7efbc146ad110956a207ddae1
Pisucho <126575169+aandandreea@users.noreply.github.com>
2025-07-12 17:23:05 +0300

    Solved Products_Ordered_in_a_Period.sql

---
+++ Products_Ordered_in_a_Period.sql
@@ -0,0 +1,46 @@
+/*1327. List the Products Ordered in a Period
+
+
+Table: Products
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| product_id       | int     |
+| product_name     | varchar |
+| product_category | varchar |
++------------------+---------+
+product_id is the primary key (column with unique values) for this table.
+This table contains data about the company's products.
+ 
+
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| order_date    | date    |
+| unit          | int     |
++---------------+---------+
+This table may have duplicate rows.
+product_id is a foreign key (reference column) to the Products table.
+unit is the number of products ordered in order_date.
+ 
+
+Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+
+Return the result table in any order.
+
+ */
+
+with cte as (
+select o.product_id,sum(o.unit) as unit
+from orders o
+where o.order_date between '2020-02-01' and '2020-02-29'
+group by product_id
+having sum(o.unit)>=100 )
+select p.product_name,cte.unit
+from products p
+inner join cte
+on p.product_id=cte.product_id
